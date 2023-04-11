878807d74cd85d8d3b245541b46a75097d2931fa
KouichiEndo <129310733+KoichiEndo323@users.noreply.github.com>
2023-04-11 09:08:04 +0000

    [Finish]課題２

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  #resource :books
+  resources :books
 
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  #post 'books' => 'books#create'
+  #get 'books/:id' => 'books#show', as: 'book'
+  #get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #patch 'books/:id' => 'books#update', as: 'update_book'
+  #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
